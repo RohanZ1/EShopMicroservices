40e7ea5d2b0a74b52b4a0374689f769bde3a8dfe
RohanZ1 <112950843+RohanZ1@users.noreply.github.com>
2024-10-27 12:33:40 +0530

    Discount Microservices Development Compeleted

---
+++ src/Services/Discount/Discount.Grpc/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Discount.Grpc.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc;
+
+    public static  class Extensions
+    {
+    public async static Task<IApplicationBuilder> UseMigration(this IApplicationBuilder app)
+    {
+        using var scope=app.ApplicationServices.CreateScope();//AS DiscountContext is scoped service injected to DI ,so we need scope to access it
+
+        using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+        await dbContext.Database.MigrateAsync();
+        return app;
+
+    }
+    }
+
