b5be9ea840f2f96b3a93e14f1a3e2157156025c7
RohanZ1 <112950843+RohanZ1@users.noreply.github.com>
2024-11-17 11:48:52 +0530

    YarpApi Gateway orcheastrated to docker environment

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,11 +1,24 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to DI containmer
 
+builder.Services.AddReverseProxy()
+                .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);//in 10 seconds , we can send maximum 5 request
+        options.PermitLimit = 5;
+    });
+});
 
 var app = builder.Build();
 
 //configure request pipeline
-
-
+app.UseRateLimiter();
+app.MapReverseProxy();
 app.Run();
